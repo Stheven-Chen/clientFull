12e4de2bfd23a8be30d49f2826f0395e8dde9a6b
Stheven <stheven4496@gmail.com>
2023-07-18 17:01:15 +0700

    style

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -163,223 +163,234 @@ const Home: React.FC = () => {
   };
   
 
-  return (
-    <div className='bg-[#cdcdcd]'>
-      <Nav />
-      {showModal && (
-        <Modal
-        text={modalText}
-        onClick={()=>setShowModal(false)}
-        />
-      )}
-      <div className="container mx-auto px-4 py-8">
-        <form onSubmit={handleSubmit} className="max-w-lg mx-auto bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-4">
-          {/* Section: Data Diri */}
-          <div className="mb-6">
-            <h2 className="text-2xl font-semibold mb-4">Data Diri</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+  return(
+  <div className="bg-[#cdcdcd]">
+    <Nav />
+    {showModal && (
+      <Modal text={modalText} onClick={() => setShowModal(false)} />
+    )}
+    <div className="container mx-auto px-4 py-8">
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-lg mx-auto bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-4"
+      >
+        <div className="mb-6">
+          <h2 className="text-2xl font-semibold mb-4">Data Diri</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <input
+              type="text"
+              name="nama"
+              value={formData.nama}
+              onChange={handleInputChange}
+              placeholder="Nama Lengkap"
+              required
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+            <div className="flex items-center gap-4">
               <input
-                type="text"
-                name="nama"
-                value={formData.nama}
+                type="radio"
+                name="jenisKelamin"
+                value="male"
                 onChange={handleInputChange}
-                placeholder="Nama Lengkap"
-                className="input-field"
                 required
+                className="text-blue-500 focus:ring-blue-500"
               />
-              <div className="flex items-center gap-4">
-                <input
-                  type="radio"
-                  name="jenisKelamin"
-                  value="male"
-                  onChange={handleInputChange}
-                  required
-                />
-                <label className="mr-4 lg:mr-0">Laki-Laki</label>
-                <input
-                  type="radio"
-                  name="jenisKelamin"
-                  value="female"
-                  onChange={handleInputChange}
-                />
-                <label>Perempuan</label>
-              </div>
+              <label className="mr-4 lg:mr-0 text-gray-600">Laki-Laki</label>
+              <input
+                type="radio"
+                name="jenisKelamin"
+                value="female"
+                onChange={handleInputChange}
+                className="text-pink-500 focus:ring-pink-500"
+              />
+              <label className="text-gray-600">Perempuan</label>
+            </div>
+            <div className="flex flex-col gap-4">
+              <label htmlFor="tanggalLahir" className="text-gray-600">
+                Tanggal Lahir
+              </label>
               <input
                 type="date"
                 name="tanggalLahir"
                 value={formData.tanggalLahir}
                 onChange={handleInputChange}
+                placeholder="Tanggal Lahir"
                 max={new Date().toISOString().split('T')[0]}
-                className="input-field"
                 required
-              />
-              <input
-                type="tel"
-                name="phone"
-                value={formData.phone}
-                onChange={handleInputChange}
-                placeholder="Nomor Telepon"
-                className="input-field"
-                required
-              />
-              <input
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleInputChange}
-                placeholder="Email"
-                className="input-field"
-                required
-              />
-              <textarea
-                name="domisili"
-                value={formData.domisili}
-                onChange={handleInputChange}
-                placeholder="Alamat Domisili"
-                rows={3}
-                className="input-field resize-none"
-                required
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
               />
             </div>
-          </div>
-
-          {/* Section: Pendidikan */}
-          <div className="mb-6">
-            <h2 className="text-2xl font-semibold mb-4">Pendidikan</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <select
-                name="pendTerakhir"
-                value={formData.pendTerakhir}
-                onChange={handleInputChange}
-                className="input-field"
-                required
-              >
-                <option value="">Pilih Jenjang Pendidikan</option>
-               {pendidikan.map((item:{name:string, value:string})=>(
-                <option key={item.value} value={item.value}>{item.name}</option>
-               ))}
-              </select>
-              <input
-                type="text"
-                name="univ"
-                value={formData.univ}
-                onChange={handleInputChange}
-                placeholder="Universitas"
-                className="input-field"
-                required
-              />
-              <input
-                type="text"
-                name="jurusan"
-                value={formData.jurusan}
-                onChange={handleInputChange}
-                placeholder="Jurusan"
-                className="input-field"
-                required
-              />
-              <input
-                type="text"
-                name="ipk"
-                value={formData.ipk}
-                onChange={handleInputChange}
-                placeholder="GPA"
-                className="input-field"
-                required
-              />
-            </div>
-          </div>
-
-          {/* Section: Pengalaman Kerja */}
-          <div className="mb-6">
-            <h2 className="text-2xl font-semibold mb-4">Pengalaman Kerja</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <input
-                type="text"
-                name="perusahaan"
-                value={formData.perusahaan}
-                onChange={handleInputChange}
-                placeholder="Perusahaan Terakhir"
-                className="input-field"
-                required
-              />
-              <input
-                type="text"
-                name="posisiT"
-                value={formData.posisiT}
-                onChange={handleInputChange}
-                placeholder="Posisi Terakhir"
-                className="input-field"
-                required
-              />
-              <select
-                name="posisi"
-                value={formData.posisi}
-                onChange={handleInputChange}
-                className="input-field"
-                required
-              >
-                <option value="">Pilih Posisi yang Dilamar</option>
-                {posisi.map((item:{name:string, value:string})=>(
-                    <option key={item.value} value={item.value}>{item.name}</option>
-                ))}
-              </select>
-              <select
-                name="lokasi"
-                value={formData.lokasi}
-                onChange={handleInputChange}
-                className="input-field"
-                required
-              >
-                <option value="">Pilih Lokasi</option>
-               {lokasiKerja.map((item:{name:string, value:string})=>(
-                <option key={item.value} value={item.value}>{item.name}</option>
-               ))}
-              </select>
-            </div>
-          </div>
-
-          <div className="mb-6 flex items-center">
-            <label className="mr-4">Sumber Lowongan:</label>
-            <select
-              name="sumber"
-              value={formData.sumber}
-              onChange={(e) => {
-                handleInputChange(e);
-                setShowReferral(e.target.value === 'referral');
-              }}
-              className="input-field"
+            <input
+              type="tel"
+              name="phone"
+              value={formData.phone}
+              onChange={handleInputChange}
+              placeholder="Nomor Telepon"
               required
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              placeholder="Email"
+              required
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+            <textarea
+              name="domisili"
+              value={formData.domisili}
+              onChange={handleInputChange}
+              placeholder="Alamat Domisili"
+              rows={3}
+              required
+              className="w-full px-4 py-2 border border-gray-300 rounded-md resize-none focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+          </div>
+        </div>
+
+        <div className="mb-6">
+          <h2 className="text-2xl font-semibold mb-4">Pendidikan</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <select
+              name="pendTerakhir"
+              value={formData.pendTerakhir}
+              onChange={handleInputChange}
+              required
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
             >
-              <option value="">Pilih Sumber Lowongan</option>
-              <option value="job_fair">Job Fair</option>
-              <option value="job_portal">Job Portal</option>
-              <option value="referral">Referral</option>
+              <option value="">Pilih Jenjang Pendidikan</option>
+              {pendidikan.map((item) => (
+                <option key={item.value} value={item.value}>
+                  {item.name}
+                </option>
+              ))}
+            </select>
+            <input
+              type="text"
+              name="univ"
+              value={formData.univ}
+              onChange={handleInputChange}
+              placeholder="Universitas"
+              required
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+            <input
+              type="text"
+              name="jurusan"
+              value={formData.jurusan}
+              onChange={handleInputChange}
+              placeholder="Jurusan"
+              required
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+            <input
+              type="text"
+              name="ipk"
+              value={formData.ipk}
+              onChange={handleInputChange}
+              placeholder="GPA"
+              required
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+          </div>
+        </div>
+
+        <div className="mb-6">
+          <h2 className="text-2xl font-semibold mb-4">Pengalaman Kerja</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <input
+              type="text"
+              name="perusahaan"
+              value={formData.perusahaan}
+              onChange={handleInputChange}
+              placeholder="Perusahaan Terakhir"
+              required
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+            <input
+              type="text"
+              name="posisiT"
+              value={formData.posisiT}
+              onChange={handleInputChange}
+              placeholder="Posisi Terakhir"
+              required
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+            <select
+              name="posisi"
+              value={formData.posisi}
+              onChange={handleInputChange}
+              required
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            >
+              <option value="">Pilih Posisi yang Dilamar</option>
+              {posisi.map((item) => (
+                <option key={item.value} value={item.value}>
+                  {item.name}
+                </option>
+              ))}
+            </select>
+            <select
+              name="lokasi"
+              value={formData.lokasi}
+              onChange={handleInputChange}
+              required
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            >
+              <option value="">Pilih Lokasi</option>
+              {lokasiKerja.map((item) => (
+                <option key={item.value} value={item.value}>
+                  {item.name}
+                </option>
+              ))}
             </select>
           </div>
+        </div>
 
-          {showReferral && (
-            <div className="mb-6">
-              <input
-                type="text"
-                name="referal"
-                value={formData.referal}
-                onChange={handleInputChange}
-                placeholder="Referal Nama"
-                className="input-field"
-                required
-              />
-            </div>
-          )}
-
-          <button
-            type="submit"
-            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-full transform-gpu active:scale-90 duration-300 w-full"
+        <div className="mb-6 flex items-center">
+          <label className="mr-4 text-gray-600">Sumber Lowongan:</label>
+          <select
+            name="sumber"
+            value={formData.sumber}
+            onChange={(e) => {
+              handleInputChange(e);
+              setShowReferral(e.target.value === 'referral');
+            }}
+            required
+            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
           >
-            Submit
-          </button>
-        </form>
-      </div>
+            <option value="">Pilih Sumber Lowongan</option>
+            <option value="job_fair">Job Fair</option>
+            <option value="job_portal">Job Portal</option>
+            <option value="referral">Referral</option>
+          </select>
+        </div>
+
+        {showReferral && (
+          <div className="mb-6">
+            <input
+              type="text"
+              name="referal"
+              value={formData.referal}
+              onChange={handleInputChange}
+              placeholder="Referal Nama"
+              required
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+          </div>
+        )}
+
+        <button
+          type="submit"
+          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-full transform-gpu active:scale-90 duration-300 w-full"
+        >
+          Submit
+        </button>
+      </form>
     </div>
+  </div>
   );
 };
 
