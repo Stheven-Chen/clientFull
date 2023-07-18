2a5e9e608217d72eb337ad1521b469f729c02fb6
Stheven <stheven4496@gmail.com>
2023-07-18 15:33:25 +0700

    remove port 3000

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 3000, // Ganti dengan port yang Anda inginkan
+    host: '0.0.0.0'
   },
 });
